fc8c8fe1cbe97d1032ac4a8df2b454f389feb70f
Neloy <19neloyk@lrei.org>
2021-05-07 16:59:13 -0400

    Started react native app for popup; implemented first component Joke

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Joke from './components/joke'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>I just dids!</Text>
+      <Joke />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/joke.js
@@ -0,0 +1,13 @@
+import React, {Component, View} from 'react';
+import {Text} from 'react-native'
+
+class Joke extends Component {
+    render() {
+        return (
+            <Text> What does the fox say? </Text>
+        )
+    }
+}
+
+
+export default Joke;
